f247b2fc6972b4aad8ffb6db1898b299c4ed8bef
Ivan Berestianyi <berestvania@gmail.com>
2024-09-07 17:31:26 +0300

    add serializer for registration

---
+++ src/users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'password']
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        user.is_active = False
+        user.save()
+        return user
+
