fb3dd5359731eeee104c6f17eaa2e5cdfb1a4b56
Ivan Berestianyi <berestvania@gmail.com>
2024-09-07 19:17:01 +0300

    add url for crud

---
+++ src/events/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from events.views import EventListCreateView, EventDetailView
+
+urlpatterns = [
+    path('events/', EventListCreateView.as_view(), name='event-list'),
+    path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
+]
