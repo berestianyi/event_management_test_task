02ab3adba7f0ffb5a05d1d06cbf6b368259a9983
Ivan Berestianyi <berestvania@gmail.com>
2024-09-09 10:50:00 +0300

    added urls

--- src/users/urls.py
+++ src/users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegistrationView.as_view(), name='register'),
-    path('confirm-email/<str:uidb64>/<str:token>/', ConfirmEmailView.as_view(), name='confirm-email'),
+    path('register/confirm-email/<str:uidb64>/<str:token>/', ConfirmEmailView.as_view(), name='confirm_email'),
 ]
