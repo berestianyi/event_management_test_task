d64491f08eece01c46a8bf8a31103954dd3fe504
Ivan Berestianyi <berestvania@gmail.com>
2024-09-09 10:49:28 +0300

    added new urls

--- src/events/urls.py
+++ src/events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from events.views import EventListCreateView, EventDetailView
+from events.views import EventListCreateView, EventDetailView, EventRegistrationView
 
 urlpatterns = [
-    path('events/', EventListCreateView.as_view(), name='event-list'),
-    path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
+    path('events/', EventListCreateView.as_view(), name='event_list'),
+    path('events/<int:pk>/', EventDetailView.as_view(), name='event_detail'),
+    path('events/register/', EventRegistrationView.as_view(), name='event_register'),
 ]
